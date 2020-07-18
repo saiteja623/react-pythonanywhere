8ad6c029c05ac7df7ec7b5e4cd275cde0193c6fa
saiteja623 <saiteja.balla13@gmail.com>
2020-07-18 16:35:47 +0530

    first commit

---
+++ news/awesome/src/index.js
@@ -0,0 +1,4 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Routing from "./instaclone/route";
+ReactDOM.render(<Routing />, document.getElementById("root"));
---
+++ news/awesome/src/instaclone/route.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+class Routing extends Component {
+  state = {};
+  render() {
+    return "hello there";
+  }
+}
+
+export default Routing;
